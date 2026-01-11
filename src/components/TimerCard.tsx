6997f0ee5db0af44cfe91b94906936b582929978
Yitchak <Yitchak@users.noreply.github.com>
2026-01-11 19:09:16 +0700

    Center alignment of timer display and start button side-by-side with gap-8, update to v1.1.68

--- src/components/TimerCard.tsx
+++ src/components/TimerCard.tsx
@@ -447,8 +447,8 @@ const TimerCard: React.FC<TimerCardProps> = ({
 
             {/* Big Timer Display */}
             {/* Big Timer Display with Inline Action */}
-            <div className="timer-display flex items-center justify-between gap-4">
-                <div className="flex flex-col">
+            <div className="timer-display flex items-center justify-center gap-8">
+                <div className="flex flex-col items-end">
                     <span className="time-value">
                         {formatTime(timeLeft)}
                     </span>
